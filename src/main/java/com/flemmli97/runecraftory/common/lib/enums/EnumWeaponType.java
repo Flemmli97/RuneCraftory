a5bf31e5d42e8f9556ecd5ba74eaa49d4709e723
Flemmli97 <Flemmli97@users.noreply.github.com>
2019-11-16 12:39:35 +0100

    weapon aoe change

--- src/main/java/com/flemmli97/runecraftory/common/lib/enums/EnumWeaponType.java
+++ src/main/java/com/flemmli97/runecraftory/common/lib/enums/EnumWeaponType.java
@@ -3,12 +3,12 @@ package com.flemmli97.runecraftory.common.lib.enums;
 public enum EnumWeaponType
 {
     FARM("farm", 3.0f, 0), 
-    SHORTSWORD("short", 2.5f, 10), 
-    LONGSWORD("long", 4.0f, 35), 
-    SPEAR("spear", 5.0f, 5), 
+    SHORTSWORD("short", 2.5f, 6), 
+    LONGSWORD("long", 4.0f, 25), 
+    SPEAR("spear", 5.0f, 1.5f), 
     HAXE("haxe", 4.5f, 40), 
-    DUAL("dual", 3.0f, 15), 
-    GLOVE("glove", 3.0f, 5), 
+    DUAL("dual", 3.0f, 10), 
+    GLOVE("glove", 3.0f, 3), 
     STAFF("staff", 3.5f, 0);
     
     private String id;
