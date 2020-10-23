5b5485f1a54c5336383e3a5e900a23ae3464deba
Flemmli97 <Flemmli97@users.noreply.github.com>
2020-10-23 15:07:45 +0200

    fix gitignore

---
+++ src/main/java/com/flemmli97/runecraftory/mobs/libs/LibEntities.java
@@ -0,0 +1,26 @@
+package com.flemmli97.runecraftory.mobs.libs;
+
+import com.flemmli97.runecraftory.RuneCraftory;
+import net.minecraft.util.ResourceLocation;
+
+public class LibEntities {
+
+    public static final ResourceLocation gate = new ResourceLocation(RuneCraftory.MODID, "gate");
+
+    public static final ResourceLocation wooly = new ResourceLocation(RuneCraftory.MODID, "wooly");
+    public static final ResourceLocation orc = new ResourceLocation(RuneCraftory.MODID, "orc");
+    public static final ResourceLocation ant = new ResourceLocation(RuneCraftory.MODID, "ant");
+    public static final ResourceLocation beetle = new ResourceLocation(RuneCraftory.MODID, "beetle");
+    public static final ResourceLocation big_muck = new ResourceLocation(RuneCraftory.MODID, "big_muck");
+    public static final ResourceLocation buffamoo = new ResourceLocation(RuneCraftory.MODID, "buffamoo");
+    public static final ResourceLocation chipsqueek = new ResourceLocation(RuneCraftory.MODID, "chipsqueek");
+    public static final ResourceLocation cluckadoodle = new ResourceLocation(RuneCraftory.MODID, "cluckadoodle");
+    public static final ResourceLocation pomme_pomme = new ResourceLocation(RuneCraftory.MODID, "pomme_pomme");
+
+    public static final ResourceLocation ambrosia = new ResourceLocation(RuneCraftory.MODID, "ambrosia");
+
+    public static final ResourceLocation ambrosia_sleep = new ResourceLocation(RuneCraftory.MODID, "sleep_ball");
+    public static final ResourceLocation ambrosia_wave = new ResourceLocation(RuneCraftory.MODID, "ambrosia_wave");
+    public static final ResourceLocation butterfly = new ResourceLocation(RuneCraftory.MODID, "butterfly");
+
+}
---
+++ src/main/java/com/flemmli97/runecraftory/mobs/libs/LibEntityConstants.java
@@ -0,0 +1,6 @@
+package com.flemmli97.runecraftory.mobs.libs;
+
+public class LibEntityConstants {
+
+    public static final int baseLevel = 5;
+}
