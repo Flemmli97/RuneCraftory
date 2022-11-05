83076ea7211fced123cd74e848bd87d3448cef4f
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-11-05 13:10:20 +0100

    teleport spell fixes

--- common/src/main/java/io/github/flemmli97/runecraftory/common/spells/TeleportSpell.java
+++ common/src/main/java/io/github/flemmli97/runecraftory/common/spells/TeleportSpell.java
@@ -1,11 +1,14 @@
 package io.github.flemmli97.runecraftory.common.spells;
 
 import io.github.flemmli97.runecraftory.api.Spell;
+import io.github.flemmli97.runecraftory.common.entities.BaseMonster;
+import io.github.flemmli97.runecraftory.common.entities.npc.EntityNPCBase;
 import net.minecraft.core.GlobalPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.TicketType;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
@@ -15,11 +18,15 @@ import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.entity.EntityTypeTest;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.Optional;
 
 public class TeleportSpell extends Spell {
+
+    public static final TicketType<Integer> LINGERING_TELEPORT_CHUNK = TicketType.create("lingering_teleport_chunk", Integer::compareTo, 5);
 
     @Override
     public void update(Player player, ItemStack stack) {
@@ -53,8 +60,7 @@ public class TeleportSpell extends Spell {
                 if (mob.level.dimension() != levelKey) {
                     ServerLevel serverLevel = mob.getServer().getLevel(levelKey);
                     if (serverLevel != null) {
-                        mob.changeDimension(serverLevel);
-                        this.safeTeleportTo(mob, home.x(), home.y(), home.z());
+                        this.changeDimension(mob, serverLevel, home.x(), home.y(), home.z());
                         return true;
                     }
                 }
@@ -79,9 +85,7 @@ public class TeleportSpell extends Spell {
             if (player.level.dimension() != levelKey) {
                 ServerLevel serverLevel = player.getServer().getLevel(levelKey);
                 if (serverLevel != null) {
-                    player.respawn();
-                    player.changeDimension(serverLevel);
-                    this.safeTeleportTo(player, home.x(), home.y(), home.z());
+                    this.changeDimension(player, serverLevel, home.x(), home.y(), home.z());
                     return true;
                 }
             }
@@ -92,6 +96,7 @@ public class TeleportSpell extends Spell {
     }
 
     private void safeTeleportTo(Entity entity, double x, double y, double z) {
+        AABB oldBox = entity.getBoundingBox();
         entity.teleportTo(x, y, z);
         entity.resetFallDistance();
         while (!entity.level.noCollision(entity) && entity.getY() < entity.level.getMaxBuildHeight()) {
@@ -102,6 +107,57 @@ public class TeleportSpell extends Spell {
             for (int i = 0; i < 32; ++i) {
                 serverLevel.sendParticles(ParticleTypes.PORTAL, entity.getX(), entity.getY() + serverLevel.random.nextDouble() * 2.0, entity.getZ(), 0, serverLevel.random.nextGaussian(), 0.0, serverLevel.random.nextGaussian(), 1);
             }
+        if (entity instanceof ServerPlayer player)
+            this.teleportNearbyImportantEntities(player, player.getLevel(), oldBox, x, y, z);
+    }
+
+    private void changeDimension(Entity entity, ServerLevel newLevel, double x, double y, double z) {
+        float yaw = entity.getYRot();
+        float pitch = entity.getXRot();
+        if (entity instanceof ServerPlayer player) {
+            player.stopRiding();
+            AABB oldBB = player.getBoundingBox();
+            ServerLevel oldLvl = player.getLevel();
+            player.teleportTo(newLevel, x, y, z, yaw, pitch);
+            if (player.isSleeping()) {
+                player.stopSleepInBed(true, true);
+            }
+            this.teleportNearbyImportantEntities(player, oldLvl, oldBB, x, y, z);
+        } else {
+            entity.unRide();
+            Entity old = entity;
+            entity = old.getType().create(newLevel);
+            if (entity != null) {
+                entity.restoreFrom(old);
+                entity.moveTo(x, y, z, yaw, pitch);
+                old.setRemoved(Entity.RemovalReason.CHANGED_DIMENSION);
+                newLevel.addDuringTeleport(entity);
+            } else
+                return;
+        }
+        while (!newLevel.noCollision(entity) && entity.getY() < newLevel.getMaxBuildHeight()) {
+            entity.setPos(entity.getX(), entity.getY() + 1.0, entity.getZ());
+        }
+        newLevel.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.MASTER, 1, 1);
+        for (int i = 0; i < 32; ++i) {
+            newLevel.sendParticles(ParticleTypes.PORTAL, entity.getX(), entity.getY() + newLevel.random.nextDouble() * 2.0, entity.getZ(), 0, newLevel.random.nextGaussian(), 0.0, newLevel.random.nextGaussian(), 1);
+        }
+    }
+
+    private void teleportNearbyImportantEntities(ServerPlayer player, ServerLevel oldLevel, AABB oldBox, double x, double y, double z) {
+        boolean crossDim = player.getLevel().dimension() != oldLevel.dimension();
+        for (Entity e : oldLevel.getEntities(EntityTypeTest.forClass(Mob.class), oldBox.inflate(24), e -> {
+            if (e instanceof BaseMonster monster)
+                return player.getUUID().equals(monster.getOwnerUUID()) && monster.behaviourState() == BaseMonster.Behaviour.FOLLOW;
+            if (e instanceof EntityNPCBase npc)
+                return player.getUUID().equals(npc.getEntityToFollowUUID());
+            return false;
+        })) {
+            if (crossDim)
+                this.changeDimension(e, player.getLevel(), x + oldLevel.random.nextDouble() * 2 - 1, y, z + oldLevel.random.nextDouble() * 2 - 1);
+            else
+                this.safeTeleportTo(e, x + oldLevel.random.nextDouble() * 2 - 1, y, z + oldLevel.random.nextDouble() * 2 - 1);
+        }
     }
 
     @Override
