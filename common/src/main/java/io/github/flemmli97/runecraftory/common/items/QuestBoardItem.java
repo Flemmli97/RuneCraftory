8cf8cb0368aca9b6e37618c753547342eb3311c9
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-08-11 22:51:16 +0200

    add wip tooltip to quest block

--- common/src/main/java/io/github/flemmli97/runecraftory/common/items/QuestBoardItem.java
+++ common/src/main/java/io/github/flemmli97/runecraftory/common/items/QuestBoardItem.java
@@ -4,10 +4,17 @@ import com.google.common.base.Suppliers;
 import com.mojang.datafixers.util.Pair;
 import io.github.flemmli97.runecraftory.common.blocks.BlockQuestboard;
 import io.github.flemmli97.runecraftory.common.registry.ModBlocks;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -20,6 +27,12 @@ public class QuestBoardItem extends BlockItem implements MultiBlockItem {
 
     public QuestBoardItem(Block block, Properties properties) {
         super(block, properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+        tooltipComponents.add(new TextComponent("WIP").withStyle(ChatFormatting.DARK_RED));
+        super.appendHoverText(stack, level, tooltipComponents, isAdvanced);
     }
 
     @Override
