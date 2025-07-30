bc153cb6bc666839a280ae0921358d37d05ca8aa
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-07-30 16:08:47 +0200

    fix null owner locking hit check in infinite loop

--- common/src/main/java/io/github/flemmli97/runecraftory/common/entities/misc/AppleProjectileEntity.java
+++ common/src/main/java/io/github/flemmli97/runecraftory/common/entities/misc/AppleProjectileEntity.java
@@ -3,24 +3,28 @@ package io.github.flemmli97.runecraftory.common.entities.misc;
 import io.github.flemmli97.runecraftory.common.registry.RuneCraftoryEntities;
 import io.github.flemmli97.runecraftory.common.utils.CombatUtils;
 import io.github.flemmli97.runecraftory.common.utils.DynamicDamage;
-import io.github.flemmli97.tenshilib.common.utils.HitResultUtils;
+import io.github.flemmli97.tenshilib.common.utils.math.OrientedBoundingBox;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 
+import java.util.List;
+
 public class AppleProjectileEntity extends BaseProjectile {
 
-    private static final EntityDataAccessor<Float> SIZE = SynchedEntityData.defineId(AppleProjectileEntity.class, EntityDataSerializers.FLOAT);
+    private static final EntityDataAccessor<Float> SCALE = SynchedEntityData.defineId(AppleProjectileEntity.class, EntityDataSerializers.FLOAT);
 
     private boolean circling;
     private int angleOffset, circleTime;
@@ -43,27 +47,27 @@ public class AppleProjectileEntity extends BaseProjectile {
     }
 
     public void withSizeInc(float size) {
-        this.entityData.set(SIZE, size);
+        this.entityData.set(SCALE, size);
     }
 
     @Override
     public int livingTickMax() {
-        return 400;
+        return 300;
     }
 
     @Override
     public float radius() {
-        return this.entityData.get(SIZE);
+        return this.getBbWidth() * this.getScale();
     }
 
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
         super.defineSynchedData(builder);
-        builder.define(SIZE, 0f);
+        builder.define(SCALE, 1f);
     }
 
     public float getScale() {
-        return this.entityData.get(SIZE) + 1;
+        return Math.max(0.2f, this.entityData.get(SCALE));
     }
 
     @Override
@@ -71,16 +75,15 @@ public class AppleProjectileEntity extends BaseProjectile {
         super.tick();
         if (!this.level().isClientSide) {
             --this.circleTime;
-            Entity owner = this.getOwner();
-            if (owner == null)
-                this.discard();
-            else if (this.circling) {
+            if (this.circling && this.getOwner() != null) {
+                Entity owner = this.getOwner();
                 if (this.circleTime > 0) {
                     Vec3 ownerPos = owner.position();
                     Vec3 pos = new Vec3(owner.getBbWidth() + 0.5, 0, 0)
                             .yRot((13 * this.livingTicks + this.angleOffset));
                     this.setDeltaMovement(ownerPos.x + pos.x() - this.getX(), ownerPos.y + this.getOwner().getBbHeight() * 0.25 - this.getY(), ownerPos.z + pos.z() - this.getZ());
                     this.hasImpulse = true;
+                    this.checkedEntities.clear();
                 } else if (this.circleTime == 0) {
                     if (owner instanceof Mob mob && mob.getTarget() != null) {
                         this.shootAtEntity(mob.getTarget(), 1, 0);
@@ -116,8 +119,22 @@ public class AppleProjectileEntity extends BaseProjectile {
     protected EntityHitResult getEntityHit(Vec3 from, Vec3 to) {
         if (!this.isAlive())
             return null;
-        if (this.attackedEntities.isEmpty())
-            return HitResultUtils.rayTraceEntities(this, from, to, this.getBoundingBox().expandTowards(this.getDeltaMovement()).inflate(1), this::canHit);
+        if (this.attackedEntities.isEmpty()) {
+            OrientedBoundingBox obb = new OrientedBoundingBox(new AABB(-this.radius(), -this.radius(), -this.radius(), this.radius(), this.radius(), this.radius())
+                    .expandTowards(to.subtract(from)),
+                    0, 0, this.position());
+            List<Entity> list = this.level().getEntities(this, obb.getEncompassingBox());
+            for (Entity e : list) {
+                if (this.canHit(e) && obb.intersects(e.getBoundingBox())) {
+                    AABB outer = obb.getEncompassingBox();
+                    Vec3 hit = new Vec3(Mth.clamp(e.position().x, outer.minX, outer.maxX),
+                            Mth.clamp(e.position().y, outer.minY, outer.maxY),
+                            Mth.clamp(e.position().z, outer.minZ, outer.maxZ));
+                    return new EntityHitResult(e, hit);
+                }
+            }
+            return null;
+        }
         return null;
     }
 
@@ -127,7 +144,7 @@ public class AppleProjectileEntity extends BaseProjectile {
         this.circling = compound.getBoolean("Circling");
         this.circleTime = compound.getInt("CirclingTime");
         this.angleOffset = compound.getInt("AngleOffset");
-        this.withSizeInc(compound.getFloat("Size"));
+        this.withSizeInc(compound.contains("Scale") ? compound.getFloat("Scale") : 1);
     }
 
     @Override
@@ -136,6 +153,6 @@ public class AppleProjectileEntity extends BaseProjectile {
         compound.putBoolean("Circling", this.circling);
         compound.putInt("CirclingTime", this.circleTime);
         compound.putInt("AngleOffset", this.angleOffset);
-        compound.putFloat("Size", this.entityData.get(SIZE));
+        compound.putFloat("Scale", this.entityData.get(SCALE));
     }
 }
